31a7566e5d23185670942a250fec8c4168bdc3f9
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-03-18 13:30:08 +0700

    commit

---
+++ app/src/main/java/com/nguyencodervn/klad04_constraintlayout/Chain_Flow.kt
@@ -0,0 +1,11 @@
+package com.nguyencodervn.klad04_constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chain_Flow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chain_flow)
+    }
+}
